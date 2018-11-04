2083ae54bbafc84cc2297ba4a3d22edc9c667623
Romain ODET <romain.odet@lecole-ldlc.com>
2018-11-04 19:07:48 +0100

    Adding the temperature function

--- rendu.py
+++ rendu.py
@@ -23,87 +23,110 @@
 # defintion des vars
 text = ""
 
+
 def blank(lignes):
     """docstring for blank"""
-    for i in range (lignes):
+    for i in range(lignes):
         print("")
+
 
 def pause():
     """docstring for pause"""
-    raw_input()
+    input()
+
 
 def dectobin(value):
-
     digits = []
     while value > 0:
         digits.append(value % 2)
         value = value // 2
     digits.reverse()
     print("".join([str(d) for d in digits]))
-    input()
+    pause()
+    blank(30)
+
 
 def bintodec(bin_string):
-
     result = 0
     for i in range(len(bin_string)):
         char = int(bin_string[i])
         if char == 1:
             result += 2 ** (len(bin_string) - 1 - i)
     print("The result is : ", result)
-    input()
+    pause()
+    blank(30)
+
 
 def rot13(text):
-
     tableau = list(text)
-    for i in range (len(text)):
+    for i in range(len(text)):
         tableau[i] = ord(tableau[i])
-        if ((tableau[i] >= ord("a") and tableau[i] <= ord('m')) or 
-        (tableau[i] >= ord("A") and tableau[i] <= ord("M"))):
+        if ((tableau[i] >= ord("a") and tableau[i] <= ord('m')) or
+                (tableau[i] >= ord("A") and tableau[i] <= ord("M"))):
             tableau[i] += 13
-        elif ((tableau[i] > ord("m") and tableau[i] <= ord('z')) or 
-        (tableau[i] > ord('M') and tableau[i] <= ord('Z'))):
+        elif ((tableau[i] > ord("m") and tableau[i] <= ord('z')) or
+              (tableau[i] > ord('M') and tableau[i] <= ord('Z'))):
             tableau[i] -= 13
-    for i in range (len(text)):
+    for i in range(len(text)):
         tableau[i] = chr(tableau[i])
-    print ''.join(tableau)
-    input()
+    print(''.join(tableau))
+    pause()
+    blank(30)
 
-def fartocel(temp):
-    """docstring for fartocel"""
-    
-def celtofar(temp):
-    """docstring for celtofar"""
-    
-def celtokelvin(temp):
-    """docstring for celtokelvin"""
-    
-def kelvintocel(temp):
-    """docstring for kelvintocel"""
-    
+
+def tempconvert(temp):
+    tableau = list(temp)
+    unite = tableau[len(tableau) - 1].lower()
+    del tableau[len(tableau) - 1]
+    temperature = float("".join(tableau))
+    if unite == "c":
+        print("La température saisie est : %0.2f °C" % temperature)
+        print(temperature, end='')
+        print(" °C --> %0.2f °K" % (temperature + 273.15))
+        print(temperature, end='')
+        print(" °C --> %0.2f °F" % (temperature * 1.8000 + 32.00))
+
+    elif unite == "k":
+        print("La température saisie est : %0.2f °K" % temperature)
+        print(temperature, end='')
+        print(" °K --> %0.2f °F" % ((temperature - 273.15) * 1.8000 + 32.00))
+        print(temperature, end='')
+        print(" °K --> %0.2f °C" % (temperature - 273.15))
+    elif unite == "f":
+        print("La température saisie est : %0.2f °F" % temperature)
+        print(temperature, end='')
+        print(" °F -->", round(((temperature + 459.67) * (5 / 9)), 2), " °K")
+        print(temperature, end='')
+        print(" °F --> %0.2f °C" % ((temperature - 32) / 1.8000))
+    pause()
+    blank(30)
+
+
 def currency(price):
     convert = 0.00
     tableau = list(price)
     if len(tableau) == 6:
         tableau[5] = tableau[5].lower()
-        seq = [tableau[0],tableau[1],'.',tableau[3],tableau[4]]
+        seq = [tableau[0], tableau[1], '.', tableau[3], tableau[4]]
         prix = float("".join(seq))
         if tableau[5] == "e":
             convert = prix / 0.878144
-            final = str(round(convert, 2)) + ' $'
+            final = str(round(convert, 2)) + ' $ (Taux de change au 03/11/2018)'
         elif tableau[5] == "d":
             convert = prix * 0.878144
-            final = str(round(convert, 2)) + ' €'
+            final = str(round(convert, 2)) + ' € (Taux de change au 03/11/2018)'
 
     print(final)
-    
-# Author : Antoine Scherrer <antoine.scherrer@lecol-ldlc.com>
-# Licence : GPL
-# Boucle pour permetre à l'utilsateur de convertir plusieurs nombres
+    pause()
+    blank(30)
+
+
 while True:
     # demande à l'utilisateur le type de conversion qu'il souhaite
-    print ("""Le CONVERTISSEUR""")
+    print("""Le CONVERTISSEUR""")
     print("")
-    choice = int(input(" - Tapez 1 pour convertir un nombre décimal en binaire\n - Tapez 2 pour convertir un nombre binaire vers un nombre décimal\n - Tapez 3 avoir un texte en rot13\n - Tapez 4 pour convertir des dollars en euros\n\n - Tapez 0 pour quitter\n\n Faites votre choix : "))
+    choice = int(input(
+        " - Tapez 1 pour convertir un nombre décimal en binaire\n - Tapez 2 pour convertir un nombre binaire vers un nombre décimal\n - Tapez 3 avoir un texte en rot13\n - Tapez 4 pour convertir des dollars en euros\n - Tapez 5 pour convertir une température\n\n - Tapez 0 pour quitter\n\n Faites votre choix : "))
     if choice == 1:
         value = int(input("Which number do you want to convert ?"))
         dectobin(value)
@@ -111,10 +134,15 @@ while True:
         bin_string = input("Which binary string do you want to convert ?")
         bintodec(bin_string)
     elif choice == 3:
-        text = raw_input("Tapez le texte à tourner de 13 charactères... : ")
+        text = input("Tapez le texte à tourner de 13 charactères... : ")
         rot13(text)
     elif choice == 4:
-        price = raw_input("Merci d'entrez un prix en avec la première lettre de la monaie (e ou d) à la fin du prix : 14.50e ou 08.30d : ")
+        price = input(
+            "Merci d'entrez un prix en avec la première lettre de la monaie (e ou d) à la fin du prix : 14.50e ou 08.30d : ")
         currency(price)
+    elif choice == 5:
+        temp = input(
+            "Merci d'entrez une température décimale avec son unité (Celcius =c, kelvin = k, farenheit = f) : ")
+        tempconvert(temp)
     elif choice == 0:
         exit("Interruption demandé par l'utilisateur")
